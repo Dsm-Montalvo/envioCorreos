1de3dd2098b998c689128df106f60b77cb3a12b7
Dsm-Montalvo <al222210568@gmail.com>
2024-05-30 19:12:21 -0600

    con base de datos

---
+++ Backup.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.27-MariaDB : Database - email_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`email_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `email_db`;
+
+/*Table structure for table `emails` */
+
+DROP TABLE IF EXISTS `emails`;
+
+CREATE TABLE `emails` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `subject` varchar(255) DEFAULT 'Sin Asunto',
+  `message` text DEFAULT 'Sin Mensaje',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `emails` */
+
+insert  into `emails`(`id`,`email`,`subject`,`message`) values (4,'openingsprueba@gmail.com','Asunto 1','Mensaje 1'),(5,'al222210568@gmail.com','Asunto 2','Mensaje 2'),(6,'al221811793@gmail.com','Asunto 3','Mensaje 3'),(7,'kirbyakira29@gmail.com','Asunto 4','Mensaje 4'),(8,'cosauno6324@gmail.com','Asunto 5','Mensaje 5'),(9,'d73279292@gmail.com','Asunto 6','Mensaje 6'),(10,'danielmontalvo126@gmail.com','Asunto 7','Mensaje 7');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
